a76eec5aad3566d9e85fd95ec138f82623b07600
0uawa <78313305+0uawa@users.noreply.github.com>
2023-11-11 21:50:32 +0100

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import { useNavigate } from "react-router-dom";
-import './App.css'
-import Pendu from './Pendu';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
-  // let navigate = useNavigate(); 
-  // const routeChange = () =>{ 
-  //   navigate(`./Pendu.tsx`);
-  // }
-  const [word, setword] = useState("")
-  
+  const navigate = useNavigate();
+  const routeChange = () => {
+    navigate(`/game`);
+  };
+  const [word, setword] = useState('');
 
   return (
     <>
       <p>Choose a word : </p>
-      <input id='wordChoosen'type='text' onChange={(e) => setword(e.target.value)}/>
-      <button>submit</button>
+      <input
+        id="wordChoosen"
+        type="text"
+        onChange={(e) => setword(e.target.value)}
+      />
+      <button onClick={routeChange}>submit</button>
       <p></p>
-      <Pendu/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
