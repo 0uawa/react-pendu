6a91b652b49b7c385029c8f855374726f43aabcd
0uawa <78313305+0uawa@users.noreply.github.com>
2023-11-11 21:50:40 +0100

    prettier format

--- src/Pendu.tsx
+++ src/Pendu.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function Pendu() {
-  const word : String = "voiture"
-  const [foundLetter,setFoundLetter] = useState()
-  const validLetter = (letter) =>letter in word?true:false
+  const word: string = 'voiture';
+  const [foundLetter, setFoundLetter] = useState();
+  const validLetter = (letter: string) => word.includes(letter);
+
   return (
     <>
       <p>/* mot que l'on doit deviner : {word} */</p>
       <p>{}</p>
-      <input size={1} maxLength={1}/>
+      <input size={1} maxLength={1} />
       <button>to propose</button>
     </>
-  )
+  );
 }
 
-export default Pendu
+export default Pendu;
