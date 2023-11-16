92cfe0501fc08eb1ff66dba4ef75bcd57032c73d
0uawa <78313305+0uawa@users.noreply.github.com>
2023-11-16 20:39:55 +0100

    game

--- src/Pendu.tsx
+++ src/Pendu.tsx
@@ -2,16 +2,45 @@ import { useState } from 'react';
 import './App.css';
 
 function Pendu() {
-  const word: string = 'voiture';
-  const [foundLetter, setFoundLetter] = useState();
-  const validLetter = (letter: string) => word.includes(letter);
+  const word: string[] = ['v', 'o', 'i', 't', 'u', 'r', 'e'];
+  const [letter, setLetter] = useState('');
+  const createHiddenWord = (word: string[]) => {
+    const res: string[] = [];
+    word.map(() => res.push('-'));
+    return res;
+  };
+  const [stepLink, setStepLink] = useState('1');
+
+  const [hiddenWord, setHiddenWord] = useState(createHiddenWord(word));
+  const testLetter = (letter: string) => {
+    console.log(createHiddenWord(word));
+    if (letter in word) {
+      console.log('bonne lettre');
+      setHiddenWord(
+        word.map((l, i) => {
+          if (l == letter) {
+            hiddenWord[i] = letter;
+          }
+        })
+      );
+    } else {
+      setStepLink(`${Number(stepLink) + 1}`);
+      console.log('mauvaise lettre');
+    }
+  };
 
   return (
     <>
       <p>/* mot que l'on doit deviner : {word} */</p>
-      <p>{}</p>
-      <input size={1} maxLength={1} />
-      <button>to propose</button>
+      <p>{hiddenWord.map()}</p>
+      <p>{letter}</p>
+      <input
+        id="choose"
+        size={1}
+        maxLength={1}
+        onChange={(e) => setLetter(e.target.value)}
+      />
+      <button onClick={() => testLetter(letter)}>to propose</button>
     </>
   );
 }
